c47d5d67d92734c08bf8b5061022e50bf06ad745
Khujamov Codes <151366013+khujamovcodes@users.noreply.github.com>
2024-10-29 23:47:45 +0500

    Footer, contanct

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        <small id="copyright-text">&copy; Copyright Karl Horning 2019</small>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
